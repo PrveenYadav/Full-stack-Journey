1806711649682c1d6c0063b767cb14238e03c42f
PrveenYadav <bikyyadav13@gmail.com>
2025-05-07 18:37:17 +0530

    database connection

---
+++ Backend/04_mega-project/src/index.js
@@ -0,0 +1,32 @@
+import connectDB from './db/index.js';
+
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv'
+dotenv.config()
+
+connectDB();
+
+/*
+import express from 'express'
+const app = express();
+
+// IIFE (Immediately Invoked Function Expression)
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error", (error) => {
+            console.log("App is unable to talk with databse", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("ERROR: ", error);
+        throw error
+    }
+})()
+
+*/
