c0a1a2210fd767c5156292c4ad3d6be9b62c7b54
PrveenYadav <bikyyadav13@gmail.com>
2025-07-23 18:54:59 +0530

    create blog card component

--- Full-stack Projects/Blog App/client/src/pages/Home.jsx
+++ Full-stack Projects/Blog App/client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Bloglist from '../components/Bloglist'
+import Card from '../components/Card'
 
 const Home = () => {
   return (
