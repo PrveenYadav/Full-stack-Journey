4ab7ba79cf53ccd88129882e3551013284cf340d
PrveenYadav <bikyyadav13@gmail.com>
2025-07-23 18:54:32 +0530

    Use motion for animation and list the blogs

--- Full-stack Projects/Blog App/client/src/components/Bloglist.jsx
+++ Full-stack Projects/Blog App/client/src/components/Bloglist.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import { blogCategories } from '../assets/assets'
+import { blog_data, blogCategories } from '../assets/assets'
+import { motion } from "motion/react" // motion package for animation
+import Card from './Card'
 
 const Bloglist = () => {
 
@@ -13,7 +15,9 @@ const Bloglist = () => {
                     <button onClick={()=> setMenu(item)} className={`cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}>
                         {item}
                         {menu === item && (
-                            <div className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'></div>
+                            <motion.div layoutId="underline"
+                            transition={{type: "spring", stiffness: 500, damping: 30}}
+                            className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'></motion.div>
                         )}
                         
                     </button>
@@ -21,8 +25,9 @@ const Bloglist = () => {
             ))}
         </div>
 
-        <div>
-            {/* --- blog cards --- */}
+        {/* --- blog cards --- */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 sm:mx-16 xl:mx-40'>
+            {blog_data.filter((blog) => menu === "All" ? true : blog.category === menu).map((blog) => <Card key={blog._id} blog={blog} />)}
         </div>
     </div>
   )
