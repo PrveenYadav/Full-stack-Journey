6c716e50fe64c8c380795b40242e6be0a5885cf3
PrveenYadav <bikyyadav13@gmail.com>
2025-12-12 21:47:49 +0530

    fix the config

--- Full-stack-Projects/mern-auth/server/configs/nodemailer.js
+++ Full-stack-Projects/mern-auth/server/configs/nodemailer.js
@@ -1,23 +1,11 @@
 import nodemailer from 'nodemailer';
 
-// const transporter = nodemailer.createTransport({
-//     host: process.env.SMTP_HOST,
-//     port: process.env.SMTP_PORT,
-//     secure: false,
-//     auth: {
-//         user: process.env.SMTP_USER,
-//         pass: process.env.SMTP_PASS
-//     }
-// })
-
 const transporter = nodemailer.createTransport({
-    host: 'smtp-relay.brevo.com',
-    port: 587,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
     secure: false,
     auth: {
-        user: '990890002@smtp-brevo.com',
-        pass: '9qkTSPDCKAIYO2jN'
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
     }
 })
-
-export default transporter
