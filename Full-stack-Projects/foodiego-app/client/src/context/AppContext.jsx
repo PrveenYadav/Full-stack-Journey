b6c1ceb71e737d3bc1852bcc3b90e716265b755e
Praveen Yadav <157797242+PrveenYadav@users.noreply.github.com>
2026-01-23 18:14:13 +0530

    Update backend URL in AppContext

--- Full-stack-Projects/foodiego-app/client/src/context/AppContext.jsx
+++ Full-stack-Projects/foodiego-app/client/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({children}) => {
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://backend-foodiego.onrender.com"
     const [isLoggedIn, setIsLoggedIn] = useState(false)
     const [userData, setUserData] = useState(false)
     const [authLoading, setAuthLoading] = useState(true);
@@ -131,4 +131,5 @@ export const AppContextProvider = ({children}) => {
             {children}
         </AppContext.Provider>
     )
+
 }
